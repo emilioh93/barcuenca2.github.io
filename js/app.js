86f4828b7e5da464d27c6e343ab298a5714a1e3c
Emilio Hurtado <emiliohurtado1@gmail.com>
2021-01-31 11:22:11 -0300

    trabajé en preloader

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// Preloader
+window.onload = function() {
+    var contenedor = document.getElementById("contenedorPreloader");
+    contenedor.style.visibility = "hidden";
+    contenedor.style.opacity = "0";
+}
