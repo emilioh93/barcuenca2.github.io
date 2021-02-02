991f62e5449beaa90ed52dd063ef0ea8d8752492
Emilio Hurtado <emiliohurtado1@gmail.com>
2021-02-02 17:22:58 -0300

    commit para actualizar

--- js/app.js
+++ js/app.js
@@ -4,3 +4,25 @@ window.onload = function() {
     contenedor.style.visibility = "hidden";
     contenedor.style.opacity = "0";
 }
+
+// Navbar
+function scrollEffect(pxPantalla) {
+    window.addEventListener("scroll", () => {
+        var scroll = document.documentElement.scrollTop;
+        var bgChange = document.getElementById("navScroll");
+        var marcoHamburg = document.getElementById("marcoHamburg");
+        var iconoHamburg = document.getElementById("iconoHamburg");
+
+        if (scroll > pxPantalla) {
+            bgChange.className = "navbar fixed-top navbar-expand-lg fuentes-personalizadas-scroll";
+            marcoHamburg.className = "navbar-toggler text-dark";
+            iconoHamburg.className = "fas fa-bars text-dark";
+        } else {
+            bgChange.className = "navbar fixed-top navbar-expand-lg fuentes-personalizadas";
+            marcoHamburg.className = "navbar-toggler text-light";
+            iconoHamburg.className = "fas fa-bars text-light";
+        }
+    })
+}
+
+scrollEffect(20);
